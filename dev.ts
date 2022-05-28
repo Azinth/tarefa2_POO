65b2fc82efaf902c05b6f76c56114d69b52e041d
Azinth <gsv.jesus@gmail.com>
2022-05-27 21:37:49 -0300

    first commit

---
+++ dev.ts
@@ -0,0 +1,46 @@
+class Conta{
+  constructor(
+    public numero: number,
+    public saldo: number,
+    public nome: string,
+  ){}
+
+  getSaldo(): void{
+    console.log(this.saldo)
+}
+}
+class Corrente extends Conta{
+  constructor( numero: number, saldo: number, nome: string, public limite: number,
+  ){
+    super(numero, saldo, nome)
+  }
+  
+
+  depositar(valor: number): void{
+    this.saldo += valor
+    console.log(`Olá ${this.nome}, seu deposito de ${valor} foi efetuado na conta ${this.numero}`)
+  }
+  sacar(valor: number): void{
+if (valor <= this.saldo + this.limite){
+  this.saldo -= valor
+  console.log(`Olá ${this.nome}, seu saque de ${valor} foi efetuado na conta ${this.numero}`)
+}else if( valor<=0){
+  console.log("Valor inválido")
+}else {
+  console.log(`Olá ${this.nome}, seu saque de ${valor} foi efetuado na conta ${this.numero}`)
+  console.log(`Saldo atual: ${this.saldo}`)
+}
+  }
+}
+
+
+let contas = new Corrente(25281, 1000, 'Gabriel', 500);
+
+console.log(contas.sacar(200));
+console.log(contas.getSaldo());
+console.log(contas.depositar(500));
+
+
+class Conta{
+
+}
